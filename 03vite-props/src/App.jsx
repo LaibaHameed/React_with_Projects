e9443d47faef546191412afd64739a1994a679e6
Laiba_012 <laibahameed012@gmail.com>
2023-10-11 22:32:06 +0500

    new mini project added

---
+++ 03vite-props/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import "./App.css";
+import Card from "./components/card";
+
+function App() {
+  let myobj = {
+    name: "Laiba",
+    age: 20
+  }
+
+  let myarr = [23,45,32]
+  return (
+    <>
+    <Card channel="code&coffe" anyobj={myobj} anyarr={myarr} btnText="Visit Me"/>
+    <Card channel="code&chai" anyobj={myobj} anyarr={myarr} btnText="Click Me"/>
+    </>
+  )
+}
+
+export default App;
